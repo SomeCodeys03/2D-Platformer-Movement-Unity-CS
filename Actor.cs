9991dc314b00ce4cf7cb29be1f349391561ffa36
SomeCodeys03 <58203283+SomeCodeys03@users.noreply.github.com>
2020-10-13 19:47:29 -0700

    Actor.cs, no other scripts attached
    Just a ACTOR SCRIPT

---
+++ Actor.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor
+{
+    protected void ActorStart()
+    {
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        
+    }
+    
+    private void LateUpdate()
+    {
+        
+    }
+    
+    protected void Move(short dir)
+    {
+        
+    }
+    
+    protected void Jump()
+    {
+        
+    }
+    
+    protected void Crouch(bool shouldCrouch)
+    {
+        
+    }
+    
+    public void TakeDamage()
+    {
+        
+    }
+    
+    private void CheckDead()
+    {
+        
+    }
+    
+    private void Die()
+    {
+        
+    }
+}
