98fe8abe26c995374b0fedf87f2d7487c1c78fd7
SomeCodeys03 <58203283+SomeCodeys03@users.noreply.github.com>
2020-10-19 16:59:20 -0700

    Update Actor.cs

--- Actor.cs
+++ Actor.cs
@@ -1,54 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
+using Audio;
+using Weapons;
 using UnityEngine;
 
 namespace Actors
 {
     public class Actor : MonoBehaviour
     {
-        //floats
         public float moveSpeed = 1200;
         public float maxMoveSpeed = 10;
         private float currentJumps;
         public float maxJumps = 2;
-        public float jumpForce;
+        public float jumpForce = 400;
+        public float groundCheckRad = 0.6f;
+        public float startHealth = 100;
 
-        //bools
         private bool grounded;
         private bool moving;
         private bool facingRight;
 
-        //layermasks
+        public Vector2 crouchScale;
+        private Vector2 originalScale;
+        protected Vector2 lookPos;
+
         public LayerMask whatIsGround;
 
-        //transforms
-        public Transform groundCheckPos;
-
-        //gameobjects
         public GameObject gun;
 
-        //components
+        public Color damageColor;
+        private Color originalColor;
+
+        public Gun weaponScript;
+        public HealthBar healthBar;
+
         private Rigidbody2D rb;
         private SpriteRenderer sr;
 
+        public float Health
+        {
+            get;
+            set;
+        }
+
         protected void ActorStart()
         {
+            if (gameObject == Game.instance.currentPlayer)
+                healthBar.SetMaxHealth(100);
             rb = GetComponent<Rigidbody2D>();
             sr = GetComponent<SpriteRenderer>();
+            Health = startHealth;
+            originalScale = (Vector2)transform.localScale;
+            originalColor = sr.color;
+            weaponScript = gun.GetComponent<Gun>();
         }
 
         private void FixedUpdate()
         {
             SlowDown();
-
-            //reset jumps
             if (currentJumps < maxJumps && grounded)
                 currentJumps = maxJumps;
         }
 
         private void LateUpdate()
         {
-            grounded = Physics2D.OverlapCircle(groundCheckPos.position, 0.6f, whatIsGround);
+            grounded = Physics2D.OverlapCircle(transform.position, groundCheckRad, whatIsGround);
         }
 
         private void SlowDown()
@@ -59,16 +73,15 @@ namespace Actors
             float velX = rb.velocity.x;
 
             if (velX > 0.6f)
-                rb.AddForce(moveSpeed * Time.deltaTime * -Vector2.right);
+                rb.AddForce((moveSpeed + 1000) * Time.deltaTime * -Vector2.right);
             else if (velX < -0.6f)
-                rb.AddForce(moveSpeed * Time.deltaTime * Vector2.right);
+                rb.AddForce((moveSpeed + 1000) * Time.deltaTime * Vector2.right);
         }
 
         protected void Move(short dir)
         {
             Flip(dir);
             moving = true;
-
             float velX = rb.velocity.x;
 
             if (velX < maxMoveSpeed && dir > 0)
@@ -87,6 +100,7 @@ namespace Actors
         {
             rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(jumpForce * Vector2.up);
+            AudioManager.Play("PlayerJump");
             currentJumps--;
         }
 
@@ -109,12 +123,12 @@ namespace Actors
 
         private void StartCrouch()
         {
-
+            transform.localScale = crouchScale;
         }
 
         private void StopCrouch()
         {
-
+            transform.localScale = originalScale;
         }
 
         protected void PlayerRotateGun()
@@ -130,27 +144,62 @@ namespace Actors
 
         protected void EnemyRotateGun()
         {
-            
+            if (Game.instance.currentPlayer == null || gun == null)
+                return;
+
+            Vector2 playerPos = Game.instance.currentPlayer.transform.position;
+            lookPos = playerPos - (Vector2)transform.position;
+            float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+            gun.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         }
 
         protected void Shoot()
         {
-
+            if (gun == null || !weaponScript.ready || Barrel.isExploding)
+                return;
+            weaponScript.Shoot();
+            rb.AddForce(weaponScript.recoil * -gun.transform.up, ForceMode2D.Impulse);
         }
 
-        public void TakeDamage()
+        public void TakeDamage(float damage)
         {
+            Health -= damage;
+            if (gameObject == Game.instance.currentPlayer)
+                healthBar.SetHealth((int)Health);
 
+            DamageFlash(0.2f);
+            CheckDead();
         }
 
         private void CheckDead()
         {
-
+            if (Health <= 0)
+                Die();
         }
 
         private void Die()
         {
+            Destroy(gameObject);
+            if (gameObject.name == "Player")
+                AudioManager.Play("PlayerDie");
+            else
+                AudioManager.Play("EnemyDie");
+        }
+        
+        private void DamageFlash(float length)
+        {
+            sr.color = damageColor;
+            Invoke("ResetColor", length);
+        }
 
+        private void ResetColor()
+        {
+            sr.color = originalColor;
+        }
+
+        public Rigidbody2D GetRb()
+        {
+            return rb;
         }
     }
 }
